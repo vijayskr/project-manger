fe8a4a50a69a6b4f80a7ff63842304c956f31c23
VIJAYASANKAR BALASUBRAMANIAN <vijayasankar@outlook.com>
2019-02-17 21:34:42 -0600

    Updated Library for the Karma Console Beautify Output
    Updated Library for the Karma Console Beautify Output

--- project-manager-ui/src/karma.conf.js
+++ project-manager-ui/src/karma.conf.js
@@ -7,7 +7,8 @@ module.exports = function(config) {
       require("karma-chrome-launcher"),
       require("karma-jasmine-html-reporter"),
       require("karma-coverage-istanbul-reporter"),
-      require("@angular-devkit/build-angular/plugins/karma")
+      require("@angular-devkit/build-angular/plugins/karma"),
+      require('karma-spec-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -21,7 +22,7 @@ module.exports = function(config) {
     stripPrefix: 'src/',
         moduleName: 'myAppTemplates'
 },
-    reporters: ["progress", "kjhtml"],
+    reporters: ["spec", "kjhtml"], // Updated progress to spec
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
